bb3f6a31baec305ed8e44f410b83ff305c91438d
Toru Nayuki <tnayuki@icloud.com>
2014-04-03 16:05:48 +0900

    Initial commit

---
+++ lib/net/ftp/walk.rb
@@ -0,0 +1,56 @@
+require 'net/ftp/list'
+
+class Net::FTP
+  def walk(dir, parser_class = Net::FTP::List, &block)
+    paths = [ dir ]
+
+    while paths.count > 0 do
+      path = paths.pop
+
+      self.list(path) do |e|
+        entry = parser_class.parse(e)
+
+        if entry.dir? then
+          unless ['.', '..'].include? entry.basename then
+            paths << path + '/' + entry.basename
+          end
+        else
+          yield path, entry
+        end
+      end
+    end
+  end
+
+  def du(dir, block_size = 4096, parser_class = Net::FTP::List)
+    sum = 0
+
+    walk(dir, parser_class) do |path, entry|
+      sum += (entry.filesize + block_size - 1) / block_size * block_size
+    end
+
+    sum
+  end
+
+  def mirror(remote_dir, local_dir, parser_class = Net::FTP::List)
+    queue = []
+
+    walk(remote_dir, parser_class) do |path, entry|
+      common_path = path[remote_dir.length + 1 .. -1]
+
+      remote_file = remote_dir + '/' + common_path + '/' + entry.basename
+      local_file = local_dir + '/' + common_path + '/' + entry.basename
+
+      if File.exist?(local_file)
+        next if File.size(local_file) == entry.filesize && File.mtime(local_file) == entry.mtime
+      end
+
+      queue << [remote_file, local_file, entry.mtime]
+    end
+
+    queue.each do |item|
+      getbinaryfile(item[0], item[1])
+
+      File.utime(item[2], item[2], item[1])
+    end
+  end
+end
---
+++ net-ftp-walk.gemspec
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+Gem::Specification.new do |spec|
+  spec.name          = "net-ftp-walk"
+  spec.version       = "1.0.0"
+  spec.authors       = ["Toru Nayuki"]
+  spec.email         = ["tnayuki@icloud.com"]
+  spec.summary       = "walks a FTP directory, also supports \"du\" and mirroring"
+  spec.homepage      = "https://github.com/tnayuki/net-ftp-walk/"
+  spec.license       = "MIT"
+
+  spec.files         = `git ls-files -z`.split("\x0")
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]
+
+  spec.add_runtime_dependency "net-ftp-list"
+
+  spec.add_development_dependency "bundler", "~> 1.5"
+  spec.add_development_dependency "rake"
+end
